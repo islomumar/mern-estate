bd0447cfb29a68afede92e7ff62b2ef7034c218b
islomumar <islomumar225@gmail.com>
2024-06-05 14:02:17 +0500

    create and route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
+import Home from "./pages/Home"
+import About from "./pages/About"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
+import Profile from "./pages/Profile"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 export default function App() {
   return (
-    <div className="text-red-500">salom</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signIn" element={<SignIn />} />
+        <Route path="/signUp" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
---
+++ src/pages/SignIn.jsx
@@ -0,0 +1,8 @@
+
+function SignIn() {
+  return (
+    <div>SignIn</div>
+  )
+}
+
+export default SignIn
